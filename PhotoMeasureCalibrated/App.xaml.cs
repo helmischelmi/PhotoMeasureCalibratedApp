3cb934115a26e99c80f0776307a62f7779b8ec47
helmischelmi <helmut.recher@gmx.de>
2024-10-22 22:31:20 +0200

    Add project files.

---
+++ PhotoMeasureCalibrated/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using PhotoMeasureCalibrated.Models;
+using PhotoMeasureCalibrated.ViewModels;
+
+namespace PhotoMeasureCalibrated
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var model = new MainModel();
+            var mainViewModel = new MainViewModel(model);
+            var mainWindow = new MainWindow {DataContext = mainViewModel};
+
+            //LogginUtils.Init();
+
+            mainWindow.Show();
+        }
+    }
+
+}
---
+++ PhotoMeasureCalibrated/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PhotoMeasureCalibrated.Models;
+
+namespace PhotoMeasureCalibrated.ViewModels;
+
+internal class MainViewModel: ObservableObject
+{
+    public MainModel Model { get; }
+
+    public MainViewModel(MainModel model)
+    {
+        Model = model;
+    }
+
+
+}
